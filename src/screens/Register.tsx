9e4a5b33978fb106c3e061ee96606bf910ed939e
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-27 12:50:32 -0300

    feat: add Register screen

---
+++ src/screens/Register.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+import { FormInput } from '../components/FormInput.tsx';
+import { ActionButton } from '../components/ActionButton.tsx';
+
+import '../styles/shared/LoginRegister.css';
+
+export function Register() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confPassword, setConfPassword] = useState('');
+
+  function onRegisterHandler() {}
+
+  return (
+    <div id="register-screen" className="login-register-shared">
+      <div className="login-wrapper">
+        <header>
+          <span><span>S</span>ignex</span>
+        </header>
+
+        <form>
+          <div>
+            <FormInput
+              label="Nome Completo"
+              value={name}
+              onChange={event => setName(event.target.value)}
+              disabled={false}/>
+            <FormInput
+              label="E-mail"
+              type="email"
+              placeholder="email@exemplo.com.br"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+              disabled={false}/>
+            <FormInput
+              label="Senha"
+              type="password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+              disabled={false}/>
+            <FormInput
+              label="Confirme a Senha"
+              type="password"
+              value={confPassword}
+              onChange={event => setConfPassword(event.target.value)}
+              disabled={false}/>
+          </div>
+
+          <ActionButton onClick={onRegisterHandler}>
+            Cadastrar
+          </ActionButton>
+        </form>
+      </div>
+    </div>
+  );
+}
