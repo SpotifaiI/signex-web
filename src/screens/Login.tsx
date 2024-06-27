68fb3a767041078d88df4f22cc17c189e90ed686
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-26 21:40:30 -0300

    feat: adjust login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { ActionButton } from '../components/ActionButton.tsx';
+import { FormInput } from '../components/FormInput.tsx';
 
 export function Login() {
   const [email, setEmail] = useState('');
@@ -11,7 +12,13 @@ export function Login() {
     <div className="login-container">
       <div className="login-wrapper">
         <form className="login-form">
-          <input/>
+          <FormInput
+            label="E-mail"
+            type="email"
+            value={2}
+            onChange={(event) => {}}
+            disabled={false}/>
+
           <ActionButton onClick={onLoginHandler}>
             Entrar
           </ActionButton>
