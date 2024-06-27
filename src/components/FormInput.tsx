0411331e2e9f2b2de51995b6ba377392487f4636
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-26 21:40:12 -0300

    feat: add FormInput

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent, HTMLInputTypeAttribute } from 'react';
+
+import '../styles/components/FormInput.css';
+
+export type FormInputProps = {
+  label: string;
+  type: HTMLInputTypeAttribute;
+  placeholder?: string;
+  value: string|number;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  disabled: boolean;
+};
+
+export function FormInput({
+  label,
+  type,
+  placeholder,
+  value,
+  onChange,
+  disabled
+}: FormInputProps) {
+  return (
+    <div className="form-input-group">
+      <label className="form-input-label">
+        {label}
+      </label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="form-input"
+        disabled={disabled}/>
+    </div>
+  );
+}
