5bb57ff75a12ec8086fd88b741378ed2c17e2367
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-27 01:23:29 -0300

    feat: add sign item do home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,15 +3,37 @@ import { Title } from '../components/Title.tsx';
 import { ActionButton } from '../components/ActionButton.tsx';
 
 export function Home() {
+  function onDeleteHandler() {
+    confirm('Confirma a exclusão do envio selecionado?');
+  }
+
   return (
     <div id="home-screen">
       <header>
-        <Title>Links</Title>
+        <Title>Envios</Title>
 
         <ActionButton onClick={() => {}}>
           Adicionar
         </ActionButton>
       </header>
+
+      <div className="sign-list">
+        <section className="sign-item">
+          <div>
+            <span>heurheuheurheuhrasd123rued</span>
+            <span>Nome do Arquivo</span>
+          </div>
+
+          <button type="button" onClick={onDeleteHandler}>Remover</button>
+
+          <div>
+            <span className="signed">contato@contato.com.br</span>
+            <span className="signed">contato@contato.com.br</span>
+            <span>contato@contato.com.br</span>
+            <span>contato@contato.com.br</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
