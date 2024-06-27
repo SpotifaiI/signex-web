32c62035196b653ff99a7c2b6cb3e434815074b0
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-26 21:40:04 -0300

    refactor: update ActionButton

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -3,17 +3,19 @@ import { PropsWithChildren } from 'react';
 import '../styles/components/ActionButton.css';
 
 export type ActionButtonProps = {
-  onClick: () => void
+  onClick: () => void;
+  disabled: boolean;
 } & PropsWithChildren;
 
 export function ActionButton({
-  onClick, children
+  onClick, children, disabled
 }: ActionButtonProps) {
   return (
     <button
       type="button"
       className="action-button-component"
-      onClick={onClick}>
+      onClick={onClick}
+      disabled={disabled}>
       {children}
     </button>
   );
