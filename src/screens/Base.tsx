d094f5300980186b84945b3f4697e81343378782
Cristian Prochnow <cristianprochnow@gmail.com>
2024-07-04 20:55:18 -0300

    feat: finish doc signing

--- src/screens/Base.tsx
+++ src/screens/Base.tsx
@@ -1,13 +1,13 @@
 import { LogOut } from 'react-feather';
 import { Outlet } from 'react-router-dom';
 
-import { useAuth } from '../contexts/Auth.tsx';
 import { ActionButton } from '../components/ActionButton.tsx';
+import { useAuth } from '../contexts/Auth.tsx';
 
 import '../styles/screens/Base.css';
 
 export function Base() {
-  const { user, logOut } = useAuth();
+  const { user, logOut, loggedIn } = useAuth();
 
   function onLogoutHandler() {
     if (confirm('Confirma logout?')) {
@@ -21,9 +21,11 @@ export function Base() {
         <div className="wrapper">
           <span><span>S</span>ignex</span>
 
-          <ActionButton onClick={onLogoutHandler}>
-            {user?.name ?? ''} <LogOut />
-          </ActionButton>
+          {loggedIn ? (
+            <ActionButton onClick={onLogoutHandler}>
+              {user?.name} <LogOut />
+            </ActionButton>
+          ) : null}
         </div>
       </header>
 
--- src/screens/Sign.tsx
+++ src/screens/Sign.tsx
@@ -1,20 +1,60 @@
-import { useNavigate, useParams } from 'react-router-dom';
-
-import '../styles/screens/Sign.css';
-import { useEffect, useState } from 'react';
-import { Title } from '../components/Title.tsx';
-import { ActionButton } from '../components/ActionButton.tsx';
+import { useCallback, useEffect, useState } from 'react';
 import { Image } from 'react-feather';
+import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
+import { Http } from '../api/Http.ts';
+import { ActionButton } from '../components/ActionButton.tsx';
+import { Title } from '../components/Title.tsx';
+
+import '../styles/screens/Sign.css';
+
 type SignParams = {
-  hash?: string
+  hash?: string;
+  signer_id: string;
+};
+
+type SignResponse = {
+  file: string;
+  is_signed: string|null
 };
 
 export function Sign() {
-  const { hash } = useParams<SignParams>();
+  const { hash, signer_id } = useParams<SignParams>();
   const navigate = useNavigate();
-  const [signCode, setSignCode] = useState('');
+  const [file, setFile] = useState('');
+  const [isSigned, setSigned] = useState(false);
+
+  const loadSign = useCallback(async () => {
+    try {
+      toast.loading('Carregando assinatura...');
+
+      const http = new Http();
+
+      const response = await http
+        .to(`/sign/search`)
+        .post({
+          hash,
+          signer_id
+        });
+
+      if (!response.isOk()) {
+        throw response.getMessage();
+      }
+
+      const signResponse: SignResponse = response.getData();
+
+      toast.dismiss();
+
+      if (signResponse) {
+        setFile(signResponse.file);
+        setSigned(signResponse.is_signed === '1');
+      }
+    } catch (exception) {
+      toast.dismiss();
+      toast.error(<>{exception}</>);
+    }
+  }, [hash, signer_id]);
 
   useEffect(() => {
     if (!hash) {
@@ -24,11 +64,9 @@ export function Sign() {
     }
 
     loadSign();
-  }, []);
+  }, [loadSign, hash, navigate]);
 
-  async function loadSign() {}
-
-  function onSendHandler(): void {
+  async function onSendHandler() {
     try {
       const code = prompt(
         'Insira o código que estava no e-mail que continha esse link:'
@@ -37,7 +75,27 @@ export function Sign() {
       if (!code) {
         throw 'Insira um código para realizar a assinatura.';
       }
+
+      const http = new Http();
+
+      toast.loading('Concluindo assinatura...');
+
+      const response = await http
+        .to(`/sign/finish`)
+        .post({
+          signer_id,
+          code
+        });
+
+      if (!response.isOk()) {
+        throw response.getMessage();
+      }
+
+      toast.dismiss();
+      toast.success('Assinatura finalizada.');
+      loadSign();
     } catch (exception) {
+      toast.dismiss();
       toast.error(<>{exception}</>);
     }
   }
@@ -49,7 +107,7 @@ export function Sign() {
       </header>
 
       <section>
-        <object type="application/pdf" data="">
+        <object type="application/pdf" data={`${import.meta.env.VITE_API_ENDPOINT}/public/${file}`}>
           <Image/>
 
           <span>Carregando arquivo...</span>
@@ -57,8 +115,8 @@ export function Sign() {
       </section>
 
       <footer>
-        <ActionButton onClick={onSendHandler}>
-          Assinar
+        <ActionButton onClick={onSendHandler} disabled={isSigned}>
+          {isSigned ? 'Assinatura Concluída!' : 'Assinar'}
         </ActionButton>
       </footer>
     </div>
